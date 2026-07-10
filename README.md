4fa4f85e8e01f92028890f4c4dbd41682f83b6e9
Manrojsarang <manrojsarang@gmail.com>
2026-07-10 12:23:15 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,195 @@
+#  Medical Appointment No-Show Prediction
+
+##  Project Overview
+
+Missed medical appointments (No-Shows) create challenges for healthcare providers by increasing waiting times, wasting medical resources, and reducing operational efficiency. This project uses Machine Learning to predict whether a patient is likely to miss a scheduled appointment based on demographic, medical, and appointment-related information.
+
+A **Random Forest Classifier** was trained, evaluated, and deployed through a **Streamlit web application**, allowing users to enter patient information and receive real-time predictions.
+
+---
+
+##  Problem Statement
+
+Healthcare providers frequently face appointment no-shows, which lead to:
+
+1. Wasted appointment slots
+2. Increased healthcare costs
+3. Longer waiting lists
+4. Reduced hospital efficiency
+
+The objective of this project is to predict whether a patient will attend or miss a scheduled appointment so hospitals can take preventive actions such as sending reminders or rescheduling appointments.
+
+---
+
+## Dataset
+
+1. **Dataset: **Medical Appointment No-Show Dataset
+2. Total Records: 110,527
+3. Type: Binary Classification
+
+### Features Used
+
+* Gender
+* Age
+* Scholarship
+* Hypertension
+* Diabetes
+* Alcoholism
+* Handicap
+* SMS Received
+* Waiting Days
+* Scheduled Hour
+* Appointment Weekday
+* Neighbourhood
+
+**Target Variable**
+
+* No-Show (Attend / Miss Appointment)
+
+---
+
+## Project Workflow
+
+1. Data Collection
+2. Exploratory Data Analysis (EDA)
+3. Data Cleaning
+4. Feature Engineering
+5. Data Preprocessing
+6. Model Training
+7. Hyperparameter Tuning
+8. Model Evaluation
+9. Feature Importance Analysis
+10. Model Serialization using Joblib
+11. Streamlit Deployment
+
+---
+
+##  Technologies Used
+
+* Python
+* Pandas
+* NumPy
+* Scikit-learn
+* Seaborn
+* Plotly
+* Streamlit
+* Joblib
+* JupyterLab
+
+---
+
+##  Machine Learning Model
+
+Several classification algorithms were explored, and the final deployed model is:
+
+**Random Forest Classifier (Hyperparameter Tuned using GridSearchCV)**
+
+The model was evaluated using:
+
+* Accuracy
+* Precision
+* Recall
+* F1-Score
+* Confusion Matrix
+
+Special attention was given to precision and recall because the dataset is imbalanced, making accuracy alone insufficient for evaluating performance.
+
+---
+
+## Deployment
+
+The trained model was saved using **Joblib** and deployed with **Streamlit**.
+
+The application allows users to:
+
+* Enter patient information
+* Predict appointment attendance
+* View prediction probabilities
+* Display feature importance
+* Provide an interactive user interface for real-time predictions
+
+---
+
+##  Project Structure
+
+```text
+medical-appointment-noshow/
+│
+├── app/
+│   └── app.py
+│
+├── data/
+│
+├── models/
+│   ├── medical_no_show_model.pkl
+│   ├── feature_columns.pkl
+│   └── feature_importance.csv
+│
+├── notebooks/
+│
+├── requirements.txt
+└── README.md
+
+```
+
+---
+
+##  How to Run
+
+1. Clone the repository.
+
+2. Install dependencies.
+
+```bash
+pip install -r requirements.txt
+```
+
+3. Start the Streamlit application.
+
+```bash
+streamlit run app/app.py
+```
+
+---
+
+## Future Improvements
+
+* Improve recall for minority class predictions.
+* Experiment with XGBoost and LightGBM.
+* Build a REST API using FastAPI.
+* Deploy the application to the cloud.
+* Add patient authentication and database integration.
+
+---
+
+##  Author
+
+**Manrojpreet**
+
+Medical appointment no show prediction Project
+
+Developed using Python, Scikit-learn, and Streamlit.
+
+## Application Screenshots
+
+### Home Page
+
+![Home](assets/screenshot(62).png)
+
+---
+
+### Attend Prediction
+
+![Attend Prediction](assets/screenshot(68).png)
+
+---
+
+### No-Show Prediction
+
+![No Show Prediction](assets/screenshot(65).png)
+
+---
+
+### Feature Importance
+
+![Feature Importance](assets/screenshot(66).png)
---
+++ app/app.py
@@ -0,0 +1,297 @@
+import plotly.express as px
+import streamlit as st
+import joblib
+import pandas as pd
+
+# ---------- Page Config ----------
+st.set_page_config(
+    page_title="Medical Appointment Predictor",
+    page_icon="🏥",
+    layout="centered"
+)
+
+# ---------- Load Model ----------
+model = joblib.load("../models/medical_no_show_model.pkl")
+feature_columns = joblib.load("../models/feature_columns.pkl")
+feature_importance = pd.read_csv("../models/feature_importance.csv")
+
+
+# ---------- Sidebar ----------
+
+st.sidebar.title(" Medical Appointment Predictor")
+
+st.sidebar.markdown("---")
+
+st.sidebar.subheader(" Model")
+st.sidebar.success("Random Forest (Tuned)")
+
+st.sidebar.subheader(" Dataset")
+st.sidebar.info("110,527 Medical Appointments")
+
+st.sidebar.subheader(" Model Accuracy")
+st.sidebar.metric("Accuracy", "79.74%")
+
+st.sidebar.subheader(" Purpose")
+st.sidebar.write(
+    """
+Predict whether a patient
+is likely to miss a scheduled
+medical appointment.
+"""
+)
+
+st.sidebar.markdown("---")
+
+st.sidebar.caption("Developed using")
+st.sidebar.write("• Python")
+st.sidebar.write("• Scikit-learn")
+st.sidebar.write("• Streamlit")
+st.sidebar.write("• Pandas")
+
+st.sidebar.markdown("---")
+st.sidebar.caption("Built using Streamlit & Scikit-learn")
+st.sidebar.markdown("---")
+
+st.sidebar.subheader(" Top 10 Important Features")
+
+fig = px.bar(
+    feature_importance.head(10),
+    x="Importance",
+    y="Feature",
+    orientation="h"
+)
+
+st.sidebar.plotly_chart(
+    fig,
+    use_container_width=True
+)
+
+# ---------- Main Page ----------
+st.title(" Medical Appointment No-Show Prediction")
+st.info("Fill in the patient's information below and click Predict.")
+
+# ---------- Inputs ----------
+st.subheader(" Patient Information")
+col1, col2 = st.columns(2)
+
+# Left Column
+with col1:
+
+    age = st.number_input(
+        "Age",
+        min_value=0,
+        max_value=120,
+        value=25
+    )
+
+    gender = st.selectbox(
+        "Gender",
+        ["Female", "Male"]
+    )
+
+    scholarship = st.selectbox(
+        "Scholarship",
+        ["No", "Yes"]
+    )
+
+    diabetes = st.selectbox(
+        "Diabetes",
+        ["No", "Yes"]
+    )
+
+    handicap = st.selectbox(
+        "Handicap",
+        ["No", "Yes"]
+    )
+
+    appointment_weekday = st.selectbox(
+        "Appointment Weekday",
+        [
+            "Monday",
+            "Saturday",
+            "Thursday",
+            "Tuesday",
+            "Wednesday"
+        ]
+    )
+
+# Right Column
+with col2:
+
+    waiting_days = st.number_input(
+        "Waiting Days",
+        min_value=0,
+        max_value=365,
+        value=10
+    )
+
+    sms_received = st.selectbox(
+        "SMS Received",
+        ["No", "Yes"]
+    )
+
+    hypertension = st.selectbox(
+        "Hypertension",
+        ["No", "Yes"]
+    )
+
+    alcoholism = st.selectbox(
+        "Alcoholism",
+        ["No", "Yes"]
+    )
+
+    scheduled_hour = st.number_input(
+        "Scheduled Hour",
+        min_value=0,
+        max_value=23,
+        value=10
+    )
+
+    neighbourhoods = sorted([col.replace("neighbourhood_", "")
+    for col in feature_columns
+    if col.startswith("neighbourhood_")])
+
+neighbourhood = st.selectbox(
+    "Neighbourhood",
+    neighbourhoods
+)
+
+st.write("---")
+
+# ---------- Predict Button ----------
+
+left, center, right = st.columns([1, 2, 1])
+
+with center:
+    predict = st.button("🔍 Predict", use_container_width=True)
+
+# ---------- Prediction ----------
+
+if predict:
+
+    input_data = pd.DataFrame({
+
+        "gender": [1 if gender == "Male" else 0],
+        "age": [age],
+        "scholarship": [1 if scholarship == "Yes" else 0],
+        "hypertension": [1 if hypertension == "Yes" else 0],
+        "diabetes": [1 if diabetes == "Yes" else 0],
+        "alcoholism": [1 if alcoholism == "Yes" else 0],
+        "handicap": [1 if handicap == "Yes" else 0],
+        "sms_received": [1 if sms_received == "Yes" else 0],
+        "waiting_days": [waiting_days],
+        "scheduled_hour": [scheduled_hour]
+
+    })
+
+    # Weekday Encoding
+    weekday_columns = [
+        "Monday",
+        "Saturday",
+        "Thursday",
+        "Tuesday",
+        "Wednesday"
+    ]
+
+    for day in weekday_columns:
+        input_data[f"appointment_weekday_{day}"] = 0
+
+    input_data[f"appointment_weekday_{appointment_weekday}"] = 1
+
+    # Neighbourhood Encoding
+    for col in feature_columns:
+        if col.startswith("neighbourhood_"):
+            input_data[col] = 0
+
+    neighbourhood_column = f"neighbourhood_{neighbourhood}"
+
+    if neighbourhood_column in input_data.columns:
+        input_data[neighbourhood_column] = 1
+
+    # Match training features
+    for col in feature_columns:
+        if col not in input_data.columns:
+            input_data[col] = 0
+
+    input_data = input_data[feature_columns]
+
+    # Prediction
+    prediction = model.predict(input_data)
+    probability = model.predict_proba(input_data)
+
+    attend_prob = probability[0][0]
+    no_show_prob = probability[0][1]
+
+    st.markdown("---")
+    st.header(" Prediction Result")
+
+    if prediction[0] == 1:
+
+        st.error(" Prediction: Patient is likely to MISS the appointment.")
+
+        st.metric(
+            "No-Show Risk",
+            f"{no_show_prob*100:.2f}%"
+        )
+
+        st.progress(float(no_show_prob))
+
+        st.caption(
+            f"Model Confidence: {no_show_prob*100:.2f}%"
+        )
+
+    else:
+
+        st.success(" Prediction: Patient is likely to ATTEND the appointment.")
+
+        st.metric(
+            "Attendance Probability",
+            f"{attend_prob*100:.2f}%"
+        )
+
+        st.progress(float(attend_prob))
+
+        st.caption(
+            f"Model Confidence: {attend_prob*100:.2f}%"
+        )
+
+    # Probability Chart
+
+    prob_df = pd.DataFrame({
+        "Class": ["Attend", "No Show"],
+        "Probability": [attend_prob, no_show_prob]
+    })
+
+    st.markdown(" Prediction Probabilities")
+
+    fig = px.bar(
+        prob_df,
+        x="Class",
+        y="Probability",
+        text="Probability"
+    )
+
+    fig.update_traces(
+        texttemplate="%{text:.2%}",
+        textposition="outside"
+    )
+
+    st.plotly_chart(
+        fig,
+        use_container_width=True
+    )
+
+    # Feature Importance
+
+    st.markdown(" Top 10 Important Features")
+
+    st.dataframe(
+        feature_importance.head(10),
+        use_container_width=True
+    )
+ # ---------- footer ----------
+st.markdown("---")
+
+st.caption(
+    "developed by manrojpreet--- Built with ❤️ using Streamlit"
+)
